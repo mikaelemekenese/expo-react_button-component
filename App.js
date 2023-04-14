0ac39b6d153b188a8bcc2eed87975f41bca9da02
Mikaele <mikaele@oneshot.nc>
2023-04-14 11:53:01 +1100

    [CLKP-#860qh4cjk] React Native - Edit the custom button component

--- App.js
+++ App.js
@@ -5,12 +5,12 @@ import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Formation sur React Native / Expo</Text>
+      <Text style={styles.containerText}>Formation sur React Native / Expo</Text>
       <StatusBar style="auto" />
       <Button
-        cp="#39d353"
+        cp="green"
         cs="#39d353"
-        title="Bouton"
+        title="Voir plus"
         outlined={true}
       />
     </View>
@@ -20,11 +20,10 @@ export default function App() {
 const Button = ({ cp, cs, onPress, outlined, title }) => {
 
   const buttonStyle = outlined ? {...styles.plainButton, ...styles.outlinedButton} : styles.plainButton;
-  const textStyle = outlined ? {...styles.plainText, ...styles.outlinedText} : styles.plainText;
 
   return (
-    <TouchableOpacity style={[buttonStyle, {borderColor: cp}]} onPress={onPress}>
-      <Text style={[textStyle, {color: cs}]}>{title}</Text>
+    <TouchableOpacity style={[buttonStyle, {backgroundColor: cp, borderColor: cs}]} onPress={onPress}>
+      <Text style={styles.textStyle}>{title}</Text>
     </TouchableOpacity>
   );
 }
@@ -32,25 +31,29 @@ const Button = ({ cp, cs, onPress, outlined, title }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#2f3337',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  containerText: {
+    color: 'white',
+    fontSize: 25,
+    marginHorizontal: 45,
+    textAlign: 'center',
   },
   plainButton: {
     marginTop: 25,
     paddingVertical: 14,
     paddingHorizontal: 28,
     borderRadius: 16,
-    backgroundColor: 'black',
-  },
-  plainText: {
-    fontSize: 16,
   },
   outlinedButton: {
     borderWidth: 3,
     borderColor: '#39d353',
   },
-  outlinedText: {
+  textStyle: {
+    fontSize: 16,
     fontWeight: 'bold',
+    color: 'white',
   }
 });
